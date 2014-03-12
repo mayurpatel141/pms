6f0d5346c400a2f842b2c6c0c93b4fe6bc659f21
mayur patel <mayurpatel141@gmail.com>
2014-03-12 14:13:29 +0530

    initial

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,15 @@
 class TagsController < ApplicationController
+
+  before_filter :find_tag
+
+  def index
+    list
+    render('list')
+  end
+  
+  def list
+    @tags = Tag.order("tags.username ASC").where(:project_id => @project.id)
+  end
 	def create
     @project = Project.find(params[:project_id])
     @tag = @project.tags.create(params[:tag])
@@ -10,5 +21,13 @@ class TagsController < ApplicationController
     @task = @project.tasks.find(params[:id])
     @task.destroy
     redirect_to project_path(@project)
+  end
+
+  private 
+  def find_tag
+    if params[:project_id]
+      @project = Project.find_by_id(params[:project_id])
+    end
+    
   end
 end
